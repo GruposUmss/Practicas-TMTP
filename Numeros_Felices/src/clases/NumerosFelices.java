ca99ffaeb71a9313c87afab5491ddb6af9a908fe
AdrianVallejosFlores <adrianvallejosflores@gmail.com>
2024-09-08 21:52:29 -0400

    se elimino lo redundante

--- Numeros_Felices/src/clases/NumerosFelices.java
+++ Numeros_Felices/src/clases/NumerosFelices.java
@@ -1,21 +1,5 @@
 package clases;
 
 public class NumerosFelices {
-	int num = 123;
-	int num2 = 444;
-	int num3 = 555;
-	String cad = "hola";
-	String cad2 = "mundo";
 	
-	double camtidad = 2.2;
-	double camtidad23 = 23.23;
-	
-	double camtidad234 = 234.234;
-	
-	public static void main(String [] args) {
-	}
-	
-	public String cadena() {
-		return cad;
-	}
 }
