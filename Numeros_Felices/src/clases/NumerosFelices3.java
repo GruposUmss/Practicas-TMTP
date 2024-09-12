4b03553c35949587773b79c0930f074f28343c3c
7Jhean <jheantrabajo@gmail.com>
2024-09-12 01:37:03 -0400

    se agrego la clase numero 3

---
+++ Numeros_Felices/src/clases/NumerosFelices3.java
@@ -0,0 +1,48 @@
+package clases;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NumerosFelices3 {
+
+    public boolean esFeliz(int numero) {
+        return esFelizRecursivo(numero, new HashSet<>());
+    }
+
+    private boolean esFelizRecursivo(int numero, Set<Integer> visitados) {
+        if (numero == 1) {
+            return true;
+        }
+        if (!visitados.add(numero)) {
+            return false;
+        }
+        int siguienteNumero = sumaDeCuadradosDeDigitos(numero);
+        return esFelizRecursivo(siguienteNumero, visitados);
+    }
+
+    private int sumaDeCuadradosDeDigitos(int numero) {
+        int suma = 0;
+        while (numero > 0) {
+            int digito = numero % 10;
+            suma += digito * digito;
+            numero /= 10;
+        }
+        return suma;
+    }
+
+    public void recorrido(int rango) {
+        for (int num = 1; num <= rango; num++) {
+            if (esFeliz(num)) {
+                System.out.println(num + " es un número feliz.");
+            } else {
+                System.out.println(num + " no es un número feliz.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        NumerosFelices3 numerosFelices = new NumerosFelices3();
+        numerosFelices.recorrido(100);
+    }
+}
+
