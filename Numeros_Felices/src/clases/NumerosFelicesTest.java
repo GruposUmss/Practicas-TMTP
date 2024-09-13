53dcd8e422b86d66df8ba9016acdbffdd2395091
AdrianVallejosFlores <adrianvallejosflores@gmail.com>
2024-09-12 22:02:17 -0400

    Se agrego los test nuevos

--- Numeros_Felices/src/clases/NumerosFelicesTest.java
+++ Numeros_Felices/src/clases/NumerosFelicesTest.java
@@ -6,31 +6,41 @@ import java.time.Duration;
 
 class NumerosFelicesTest {
 	
-	@Test
-	public void testNumFel1_1() {
-		NumerosFelices1 numFel1 = new NumerosFelices1();
-		
-		numFel1.recorrido(100);
-	}
+	int rango = 100;
 	
 	@Test
-    public void testNumFel1_2() {
+    public void testNumFel1() {
         NumerosFelices1 numFel1 = new NumerosFelices1();
 
         long startTime = System.nanoTime(); 
-        numFel1.recorrido(100);
+        numFel1.recorrido(rango);
         long endTime = System.nanoTime(); 
 
         double durationNanos = (endTime - startTime) / 1_000_000_000.0;
-        System.out.printf("El tiempo de ejecucion del recorrido fue: %.9f segundos%n", durationNanos);
+        System.out.printf("El tiempo de ejecucion del recorrido 1: %.9f segundos%n", durationNanos);
     }
 	
 	@Test
-	public void testNumFel1_3() {
-		NumerosFelices1 numFel1 = new NumerosFelices1();
-		
-		assertTimeout(Duration.ofSeconds(5), () -> {
-            numFel1.recorrido(100);
-        });
-	}
+    public void testNumFel2() {
+        NumerosFelices2 numFel1 = new NumerosFelices2();
+
+        long startTime = System.nanoTime(); 
+        numFel1.recorrido(rango);
+        long endTime = System.nanoTime(); 
+
+        double durationNanos = (endTime - startTime) / 1_000_000_000.0;
+        System.out.printf("El tiempo de ejecucion del recorrido 2: %.9f segundos%n", durationNanos);
+    }
+	
+	@Test
+    public void testNumFel3() {
+        NumerosFelices3 numFel1 = new NumerosFelices3();
+
+        long startTime = System.nanoTime(); 
+        numFel1.recorrido(rango);
+        long endTime = System.nanoTime(); 
+
+        double durationNanos = (endTime - startTime) / 1_000_000_000.0;
+        System.out.printf("El tiempo de ejecucion del recorrido 3: %.9f segundos%n", durationNanos);
+    }
 }
