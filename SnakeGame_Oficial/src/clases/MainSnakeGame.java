0ec7d2791851437669b82f55b2be3808b2040148
AdrianVallejosFlores <adrianvallejosflores@gmail.com>
2024-09-29 22:02:25 -0400

    Se agrego clase main para la visualizacion inicial

---
+++ SnakeGame_Oficial/src/clases/MainSnakeGame.java
@@ -0,0 +1,14 @@
+package clases;
+import javax.swing.*;
+
+public class MainSnakeGame {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		SnakeGame game = new SnakeGame();
+		frame.add(game);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	} 
+}
